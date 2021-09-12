757b5c90f07765ba5d1bc4d892cd53f75aa633c4
Sherkhan900 <90540679+Sherkhan900@users.noreply.github.com>
2021-09-12 11:05:56 +0500

    Update Crack.py

--- Crack.py
+++ Crack.py
@@ -1,34 +1,51 @@
-9#!/usr/bin/python2
+#!/usr/bin/python2
 #coding=utf-8
-# Decompiled By Sayyed Zakarya
-# Github : https://github.com/aryanotriks
-# uncompyle6 version 3.7.4
-# Python bytecode 2.7
-# Decompiled from: Python 2.7.18 (default, Mar 20 2021, 14:58:25) 
-# [GCC 4.2.1 Compatible Android (6454773 based on r365631c2) Clang 9.0.8 (https:/
-# Embedded file name: aso
-import os, sys, time, datetime, re, threading, json, random, requests, hashlib, cookielib, uuid
+
+
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,requests,mechanize
 from multiprocessing.pool import ThreadPool
 from requests.exceptions import ConnectionError
-__author__ = 'Sayyed Zakarya'
-__copyright = 'All rights reserved . Copyright  Sayyed Zakarya'
-os.system('termux-setup-storage')
-try:
-    os.mkdir('/sdcard/ids')
-except OSError:
-    pass
+from mechanize import Browser
 
-bd = random.randint(20000000.0, 30000000.0)
-sim = random.randint(20000, 40000)
-header = {'x-fb-connection-bandwidth': repr(bd), 'x-fb-sim-hni': repr(sim), 
-   'x-fb-net-hni': repr(sim), 
-   'x-fb-connection-quality': 'EXCELLENT', 
-   'x-fb-connection-type': 'cell.CTRadioAccessTechnologyHSDPA', 
-   'user-agent': 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]', 
-   'content-type': 'application/x-www-form-urlencoded', 
-   'x-fb-http-engine': 'Liger'}
-os.system('git pull')
-os.system('clear')
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(),max_time=1)
+br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
+
+
+def keluar():
+	print "\033[1;96m[!] \x1b[1;91mExit"
+	os.sys.exit()
+
+
+def acak(b):
+    w = 'ahtdzjc'
+    d = ''
+    for i in x:
+        d += '!'+w[random.randint(0,len(w)-1)]+i
+    return cetak(d)
+
+
+def cetak(b):
+    w = 'ahtdzjc'
+    for i in w:
+        j = w.index(i)
+        x= x.replace('!%s'%i,'\033[%s;1m'%str(31+j))
+    x += '\033[0m'
+    x = x.replace('!0','\033[0m')
+    sys.stdout.write(x+'\n')
+
+
+def jalan(z):
+	for e in z + '\n':
+		sys.stdout.write(e)
+		sys.stdout.flush()
+		time.sleep(00000.1)
+
+
 logo = """
 \033[1;94m  _______ _____  _   _ _____  ___    _     _ _____ 
 \033[1;92m (_____  )  _  )( ) ( )  _  )|  _ \ ( )   ( )  _  )
